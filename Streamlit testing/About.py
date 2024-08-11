c1bea1258b072e0d1386220d49ed234b729e7a18
AKASH7358 <121285908+AKASH7358@users.noreply.github.com>
2024-08-11 12:51:20 +0530

    Add files via upload

---
+++ Streamlit testing/About.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def app():
+
+    st.header(' About Page ')
+    st.write('Information')
---
+++ Streamlit testing/Moodboard.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def app():
+
+    st.header(' Moodboard Page ')
+    st.write('Images')
