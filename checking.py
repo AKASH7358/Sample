bcf81afde6d9a5fe8105abed5e158ae74fab0a50
AKASH7358 <121285908+AKASH7358@users.noreply.github.com>
2024-08-11 16:33:51 +0530

    Update checking.py

--- checking.py
+++ checking.py
@@ -1,7 +1,15 @@
 import streamlit as st
 
 # Initialize connection.
-conn = st.connection("postgresql", type="sql")
+conn = st.connection(
+    "sql", 
+    dialect=st.secrets["postgresql"]["dialect"],
+    host=st.secrets["postgresql"]["host"],
+    port=st.secrets["postgresql"]["port"],
+    database=st.secrets["postgresql"]["database"],
+    username=st.secrets["postgresql"]["username"],
+    password=st.secrets["postgresql"]["password"]
+)
 
 # Perform query.
 df = conn.query('SELECT * FROM mytable;', ttl="10m")
