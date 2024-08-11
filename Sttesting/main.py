0477f40f80f178fef4e7eed8be8fb94f9e1003b8
AKASH7358 <121285908+AKASH7358@users.noreply.github.com>
2024-08-11 18:38:45 +0530

    Add files via upload

---
+++ Sttesting/main.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+about_page = st.Page(
+    page="views/about.py",
+    title="About",
+    icon=":material/account_circle:",
+    default=True,
+)
+moodboard_page = st.Page(
+    page = "views/moodboard.py",
+    title="Moodboard",
+    icon=":material/image:",
+)
+#project_2_page = st.Page(
+#    "views/chatbot.py",
+#    title="Chat Bot",
+ ##   icon=":material/smart_toy:",
+#)
+
+pg = st.navigation(
+    {
+        "Info": [about_page],
+        "Project": [moodboard_page],
+    }
+)
+pg.run()
