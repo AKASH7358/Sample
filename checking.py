ae3d25eff565809107019ce9f13d016ab14c028b
AKASH7358 <121285908+AKASH7358@users.noreply.github.com>
2024-08-12 13:48:21 +0530

    Update checking.py

--- checking.py
+++ checking.py
@@ -4,9 +4,9 @@ import streamlit as st
 conn = st.connection("postgresql", type="sql")
 
 # Perform query.
-df = conn.query('SELECT * FROM mytable;', ttl="10m")
+df = conn.query('SELECT * FROM prompts;', ttl="10m")
 
 # Print results.
 for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+    st.write(f"{row.sno} has a :{row.image}:")
 
